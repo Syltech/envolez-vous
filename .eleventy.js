127b63e8c1efd2bdd4ea953eb08247721533b312
Sylvain Téchené <sylvain.techene@wanadoo.fr>
2021-10-24 10:31:15 +0200

    mise en place eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
+const yaml = require("js-yaml");
+const moment = require("moment");
+moment.locale("fr");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addDataExtension("yaml", (contents) => {
+    return yaml.load(contents);
+  });
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("admin");
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return moment(dateObj).format("DD/MM/YYYY");
+  });
+  return {
+    dir: {
+      input: "src",
+    },
+    htmlTemplateEngine: "njk",
+  };
 };
